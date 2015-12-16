ae9050d3692d686f43f3c6535f160c282d6c37ef
Daniel Hansson <enoch85@gmail.com>
2015-12-16 06:21:19 +0100

    Replace echo with EOF

--- production/instruction.sh
+++ production/instruction.sh
@@ -1,24 +1,28 @@
+
 #!/bin/bash
 #
 clear
+cat << INST1
++-----------------------------------------------------------------------+
+| Thank you for downloading this ownCloud VM made by Tech and Me!       |
+|                                                                       |
+INST1
+echo -e "|"  "\e[32mTo run the startup script, just type: the sudo password\e[0m                |"
+echo -e "|"  "\e[32mThe sudo password is: owncloud, or the PW you chose during install\e[0m     |"
+cat << INST2
+|                                                                       |
+| If you never done this before you can follow the complete             |
+| install instructions: https://goo.gl/j5Vyxm                           |
+|                                                                       |
+| You can also set the ownCloud update process as a cronjob.            |
+| This is made thru the built in script in this VM that                 |
+| updates ownCloud, set secure permissions, and then                    |
+| logs the successful update in /var/log/cronjobs_success.log           |
+| Here is a guide on how to set it up:                                  |
+| https://www.en0ch.se/set-automatic-owncloud-updates/                  |
+|                                                                       |
+|  ####################### Tech and Me - 2015 #######################   |
++-----------------------------------------------------------------------+
+INST2
 
-echo "+-----------------------------------------------------------------------+"
-echo "| Thank you for downloading this ownCloud VM made by Tech and Me! |"
-echo "|                                                                         |"
-echo -e "|"     "\e[32mTo run the startup script, just type: the sudo password\e[0m             |"
-echo -e "|"     "\e[32mThe sudo password is: owncloud, or the PW you chose during install\e[0m  |"
-echo "|                                                                         |"
-echo "| If you never done this before you can follow the complete               |"
-echo "| install instructions: https://goo.gl/j5Vyxm                             |"
-echo "|                                                                         |"
-echo "| You can also set the ownCloud update process as a cronjob.              |"
-echo "| This is made thru the built in script in this VM that                   |"
-echo "| updates ownCloud, set secure permissions, and then                      |"
-echo "| logs the successful update in /var/log/cronjobs_success.log             |"
-echo "| Here is a guide on how to set it up:                                    |"
-echo "| https://www.en0ch.se/set-automatic-owncloud-updates/                    |"
-echo "|                                                                 |"
-echo "|  ####################### Tech and Me - 2015 #######################     |"
-echo "+-----------------------------------------------------------------------+"
-
-exit 1
+exit 0
