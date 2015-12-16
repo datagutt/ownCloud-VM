71860249d3849fed933c714069fe2e7d20f30f34
Daniel Hansson <enoch85@gmail.com>
2015-12-16 04:45:17 +0100

    Some more updates

--- production/owncloud-startup-script.sh
+++ production/owncloud-startup-script.sh
@@ -2,89 +2,99 @@
 #
 ## Tech and Me ## - 2015, https://www.en0ch.se/about-me
 #
-
 SCRIPTS=/var/scripts
 
-# Check if root
-if [ "$(whoami)" != "root" ]; then
+	# Check if root
+	if [ "$(whoami)" != "root" ]; then
         echo
         echo -e "\e[31mSorry, you are not root.\n\e[0mYou must type: \e[36msudo \e[0mbash $SCRIPTS/owncloud-startup-script.sh"
         echo
         exit 1
 fi
       	# Create dir
+      	if [ -d $SCRIPTS ]; then
+      		sleep 1
+      		else
       	mkdir $SCRIPTS
-
-         # The update script
-        if [ -f $SCRIPTS/owncloud_update.sh ]
-		then
-		echo "owncloud_update.sh exists"
-		else
-		wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/master/production/owncloud_update.sh -P $SCRIPTS
-
+fi
+        # Activate SSL
+        if [ -f $SCRIPTS/activate-ssl.sh ];
+                then
+                echo "activate-ssl.sh exists"
+                else
+        wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/master/lets-encrypt/activate-ssl.sh -P $SCRIPTS
+fi
+        
+        # The update script
+        if [ -f $SCRIPTS/owncloud_update.sh ];
+                then
+                echo "owncloud_update.sh exists"
+                else
+        wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/master/production/owncloud_update.sh -P $SCRIPTS
+fi
         # Sets trusted domain in when owncloud-startup-script.sh is finished
-       	if [ -f $SCRIPTS/trusted.sh ]
-		then
-		echo "trusted.sh exists"
-		else		
+        if [ -f $SCRIPTS/trusted.sh ];
+                then
+                echo "trusted.sh exists"
+                else
         wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/master/production/trusted.sh -P $SCRIPTS
-        
-		# Sets static IP to UNIX
-       	if [ -f $SCRIPTS/ip.sh ]
-		then
-		echo "ip.sh exists"
-		else
-        wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/master/production/ip.sh -P $SCRIPTS
-        
-		# Tests connection after static IP is set
-       	if [ -f $SCRIPTS/test_connection.sh ]
-		then
-		echo "test_connection.sh exists"
-		else        
-		wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/master/production/test_connection.sh -P $SCRIPTS
-        
-		# Sets secure permissions after upgrade
-       	if [ -f $SCRIPTS/setup_secure_permissions_owncloud.sh ]
-		then
-		echo "setup_secure_permissions_owncloud.sh exists"
-		else
+fi
+                # Sets static IP to UNIX
+        if [ -f $SCRIPTS/ip.sh ];
+                then
+                echo "ip.sh exists"
+                else
+      	wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/master/production/ip.sh -P $SCRIPTS
+fi
+                # Tests connection after static IP is set
+        if [ -f $SCRIPTS/test_connection.sh ];
+                then
+                echo "test_connection.sh exists"
+                else
+        wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/master/production/test_connection.sh -P $SCRIPTS
+fi
+                # Sets secure permissions after upgrade
+        if [ -f $SCRIPTS/setup_secure_permissions_owncloud.sh ];
+                then
+                echo "setup_secure_permissions_owncloud.sh exists"
+                else
         wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/master/production/setup_secure_permissions_owncloud.sh -P $SCRIPTS
-        
-		# Welcome message after login (change in /home/ocadmin/.profile
-       	if [ -f $SCRIPTS/instruction.sh ]
-		then
-		echo "instruction.sh exists"
-		else
+fi
+                # Welcome message after login (change in /home/ocadmin/.profile
+        if [ -f $SCRIPTS/instruction.sh ];
+                then
+                echo "instruction.sh exists"
+                else
         wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/master/production/instruction.sh -P $SCRIPTS
-        
-		# Clears command history on every login
-       	if [ -f $SCRIPTS/history.sh ]
-		then
-		echo "history.sh exists"
-		else
+fi
+                # Clears command history on every login
+        if [ -f $SCRIPTS/history.sh ];
+                then
+                echo "history.sh exists"
+                else
         wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/master/production/history.sh -P $SCRIPTS
-        
-		# Change roots .bash_profile
-       	if [ -f $SCRIPTS/change-root-profile.sh ]
-		then
-		echo "change-root-profile.sh exists"
-		else
+fi
+                # Change roots .bash_profile
+        if [ -f $SCRIPTS/change-root-profile.sh ];
+                then
+                echo "change-root-profile.sh exists"
+                else
         wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/master/production/change-root-profile.sh -P $SCRIPTS
-        
-		# Change ocadmin .bash_profile
-       	if [ -f $SCRIPTS/change-ocadmin-profile.sh ]
-		then
-		echo "change-ocadmin-profile.sh  exists"
-		else
+fi
+                # Change ocadmin .bash_profile
+        if [ -f $SCRIPTS/change-ocadmin-profile.sh ];
+                then
+                echo "change-ocadmin-profile.sh  exists"
+                else
         wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/master/production/change-ocadmin-profile.sh -P $SCRIPTS
-        
-		# Get startup-script for root
-       	if [ -f $SCRIPTS/owncloud-startup-script.sh ]
-		then
-		echo "owncloud-startup-script.sh exists"
-		else
+fi
+                # Get startup-script for root
+        if [ -f $SCRIPTS/owncloud-startup-script.sh ];
+                then
+                echo "owncloud-startup-script.sh exists"
+                else
         wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/master/production/owncloud-startup-script.sh -P $SCRIPTS
-
+fi
         # Make $SCRIPTS excutable 
         chmod +x -R $SCRIPTS
         chown root:root -R $SCRIPTS
@@ -98,10 +108,6 @@ fi
         mv /var/scripts/index.php /var/www/html/index.php && rm -f /var/www/html/index.html
         chmod 750 /var/www/html/index.php && chown www-data:www-data /var/www/html/index.php
 
-        # Change .profile
-        sudo bash $SCRIPTS/change-root-profile.sh
-        sudo bash $SCRIPTS/change-ocadmin-profile.sh
-        
 clear
 echo "+--------------------------------------------------------------------+"
 echo "| This script will configure your ownCloud and activate SSL.         |"
@@ -241,7 +247,6 @@ clear
 Change Trusted Domain and CLI
 bash $SCRIPTS/trusted.sh
 
-
 # Change password
 echo -e "\e[0m"
 echo "For better security, change the Linux password for [ocadmin]"
@@ -291,6 +296,7 @@ bash $SCRIPTS/owncloud_update.sh
 
 # Cleanup 1
 apt-get autoremove -y
+apt-get autoclean
 CLEARBOOT=$(dpkg -l linux-* | awk '/^ii/{ print $2}' | grep -v -e `uname -r | cut -f1,2 -d"-"` | grep -e [0-9] | xargs sudo apt-get -y purge)
 echo "$CLEARBOOT"
 clear
@@ -325,7 +331,22 @@ cat /dev/null > /var/log/apache2/access.log
 cat /dev/null > /var/log/apache2/error.log
 cat /dev/null > /var/log/cronjobs_success.log
 sed -i 's|sudo -i||g' /home/ocadmin/.bash_profile
-sed -i 's|bash chown ocadmin:ocadmin $SCRIPTS/change-ocadmin-profile.sh||g' /etc/rc.local
+cat << RCLOCAL
+#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+
+exit 0
+RCLOCAL
 
 ## Reboot
 reboot
