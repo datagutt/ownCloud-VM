c2b035edf966a864fd5b5e3ecd0c1736991d342e
Daniel Hansson <enoch85@gmail.com>
2016-01-22 15:56:12 +0100

    quiet wget

--- production/owncloud-startup-script.sh
+++ production/owncloud-startup-script.sh
@@ -3,6 +3,10 @@
 ## Tech and Me ## - 2015, https://www.techandme.se/about-me
 #
 SCRIPTS=/var/scripts
+PW_FILE=/var/mysql_password.txt # Keep in sync with owncloud_install_production.sh
+IFACE="eth0"
+IFCONFIG="/sbin/ifconfig"
+ADDRESS=$($IFCONFIG $IFACE | awk -F'[: ]+' '/\<inet\>/ {print $4; exit}')
 
 	# Check if root
 	if [ "$(whoami)" != "root" ]; then
@@ -11,110 +15,61 @@ SCRIPTS=/var/scripts
         echo
         exit 1
 fi
-      	# Create dir
-      	if [ -d $SCRIPTS ]; then
-      		sleep 1
-      		else
-      	mkdir $SCRIPTS
-fi
         # Activate SSL
         if [ -f $SCRIPTS/activate-ssl.sh ];
                 then
                 echo "activate-ssl.sh exists"
                 else
-        wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/stable/lets-encrypt/activate-ssl.sh -P $SCRIPTS
+        wget -q https://raw.githubusercontent.com/enoch85/ownCloud-VM/stable/lets-encrypt/activate-ssl.sh -P $SCRIPTS
 fi
         # The update script
         if [ -f $SCRIPTS/owncloud_update.sh ];
                 then
                 echo "owncloud_update.sh exists"
                 else
-        wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/stable/production/owncloud_update.sh -P $SCRIPTS
+        wget -q https://raw.githubusercontent.com/enoch85/ownCloud-VM/stable/production/owncloud_update.sh -P $SCRIPTS
 fi
         # Sets trusted domain in when owncloud-startup-script.sh is finished
         if [ -f $SCRIPTS/trusted.sh ];
                 then
                 echo "trusted.sh exists"
                 else
-        wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/stable/production/trusted.sh -P $SCRIPTS
+        wget -q https://raw.githubusercontent.com/enoch85/ownCloud-VM/stable/production/trusted.sh -P $SCRIPTS
 fi
                 # Sets static IP to UNIX
         if [ -f $SCRIPTS/ip.sh ];
                 then
                 echo "ip.sh exists"
                 else
-      	wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/stable/production/ip.sh -P $SCRIPTS
+      	wget -q https://raw.githubusercontent.com/enoch85/ownCloud-VM/stable/production/ip.sh -P $SCRIPTS
 fi
                 # Tests connection after static IP is set
         if [ -f $SCRIPTS/test_connection.sh ];
                 then
                 echo "test_connection.sh exists"
                 else
-        wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/stable/production/test_connection.sh -P $SCRIPTS
+        wget -q https://raw.githubusercontent.com/enoch85/ownCloud-VM/stable/production/test_connection.sh -P $SCRIPTS
 fi
                 # Sets secure permissions after upgrade
         if [ -f $SCRIPTS/setup_secure_permissions_owncloud.sh ];
                 then
                 echo "setup_secure_permissions_owncloud.sh exists"
                 else
-        wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/stable/production/setup_secure_permissions_owncloud.sh -P $SCRIPTS
+        wget -q https://raw.githubusercontent.com/enoch85/ownCloud-VM/stable/production/setup_secure_permissions_owncloud.sh -P $SCRIPTS
 fi
-                # Welcome message after login (change in /home/ocadmin/.profile
-        if [ -f $SCRIPTS/instruction.sh ];
-                then
-                echo "instruction.sh exists"
-                else
-        wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/stable/production/instruction.sh -P $SCRIPTS
-fi
-                # Clears command history on every login
-        if [ -f $SCRIPTS/history.sh ];
-                then
-                echo "history.sh exists"
-                else
-        wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/stable/production/history.sh -P $SCRIPTS
-fi
-                # Change roots .bash_profile
-        if [ -f $SCRIPTS/change-root-profile.sh ];
-                then
-                echo "change-root-profile.sh exists"
-                else
-        wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/stable/production/change-root-profile.sh -P $SCRIPTS
-fi
-                # Change ocadmin .bash_profile
-        if [ -f $SCRIPTS/change-ocadmin-profile.sh ];
-                then
-                echo "change-ocadmin-profile.sh  exists"
-                else
-        wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/stable/production/change-ocadmin-profile.sh -P $SCRIPTS
-fi
-                # Get startup-script for root
-        if [ -f $SCRIPTS/owncloud-startup-script.sh ];
-                then
-                echo "owncloud-startup-script.sh exists"
-                else
-        wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/stable/production/owncloud-startup-script.sh -P $SCRIPTS
-fi
-        # Make $SCRIPTS excutable 
-        chmod +x -R $SCRIPTS
-        chown root:root -R $SCRIPTS
-
-        # Allow ocadmin to run theese scripts
-        chown ocadmin:ocadmin $SCRIPTS/instruction.sh
-        chown ocadmin:ocadmin $SCRIPTS/history.sh
-
         # Get the Welcome Screen when http://$address
         if [ -f $SCRIPTS/index.php ];
                 then
                 rm $SCRIPTS/index.php
                 else
-        wget https://raw.githubusercontent.com/enoch85/ownCloud-VM/stable/production/index.php -P $SCRIPTS
+        wget -q https://raw.githubusercontent.com/enoch85/ownCloud-VM/stable/production/index.php -P $SCRIPTS
 fi
         mv /var/scripts/index.php /var/www/html/index.php && rm -f /var/www/html/index.html
         chmod 750 /var/www/html/index.php && chown www-data:www-data /var/www/html/index.php
         
-        # Change .profile
-        bash $SCRIPTS/change-root-profile.sh
-        bash $SCRIPTS/change-ocadmin-profile.sh
+# Make $SCRIPTS excutable 
+chmod +x -R $SCRIPTS
+chown root:root -R $SCRIPTS
 
 clear
 echo "+--------------------------------------------------------------------+"
@@ -161,9 +116,6 @@ sed -i '$a deb http://download.webmin.com/download/repository sarge contrib' /et
 wget -q http://www.webmin.com/jcameron-key.asc -O- | sudo apt-key add -
 apt-get update
 apt-get install --force-yes -y webmin
-IFACE="eth0"
-IFCONFIG="/sbin/ifconfig"
-ADDRESS=$($IFCONFIG $IFACE | awk -F'[: ]+' '/\<inet\>/ {print $4; exit}')
 echo
 echo "Webmin is installed, access it from your browser: https://$ADDRESS:10000"
 sleep 2
@@ -195,9 +147,6 @@ sleep 3
 clear
 
 # Change IP
-IFACE="eth0"
-IFCONFIG="/sbin/ifconfig"
-ADDRESS=$($IFCONFIG $IFACE | awk -F'[: ]+' '/\<inet\>/ {print $4; exit}')
 echo -e "\e[0m"
 echo "The script will now configure your IP to be static."
 echo -e "\e[36m"
@@ -244,16 +193,6 @@ bash $SCRIPTS/test_connection.sh
 sleep 2
 clear
 
-# Install owncloud
-# bash $SCRIPTS/install-owncloud.sh ELLER via repo. Ändra till /var/www i installl-owncloud om det ska göras här
-#
-#
-# Ändra för att kunna lägga till redis och trusted, Trusted måste komma sist 
-# sed -e s|);||g /var/www/owncloud/config/config.php
-#
-# Install Redis
-#
-#
 # Change Trusted Domain and CLI
 bash $SCRIPTS/trusted.sh
 
@@ -320,7 +259,9 @@ echo -e "|         \e[0mLogin to ownCloud in your browser:\e[36m" $ADDRESS"\e[32
 echo    "|                                                                    |"
 echo -e "|         \e[0mPublish your server online! \e[36mhttp://goo.gl/H7IsHm\e[32m           |"
 echo    "|                                                                    |"
-echo -e "|  \e[91m################## Tech and Me - 2015-2016 ###################\e[32m    |"
+echo -e "|      \e[0mYour MySQL password is stored in: \e[36m$PW_FILE\e[32m     |"
+echo    "|                                                                    |"
+echo -e "|    \e[91m#################### Tech and Me - 2016 ####################\e[32m    |"
 echo    "+--------------------------------------------------------------------+"
 echo
 read -p "Press any key to reboot..." -n1 -s
