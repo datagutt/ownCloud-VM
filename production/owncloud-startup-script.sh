21bcbf9ba67c19fdd7cfd684dcf6ad686f8947b3
enoch85 <enoch85@gmail.com>
2016-03-13 20:11:37 +0100

    add OCPATH instead

--- production/owncloud-startup-script.sh
+++ production/owncloud-startup-script.sh
@@ -343,7 +343,8 @@ cat /dev/null > /var/spool/mail/ocadmin
 cat /dev/null > /var/log/apache2/access.log
 cat /dev/null > /var/log/apache2/error.log
 cat /dev/null > /var/log/cronjobs_success.log
-sed -i 's|sudo -i||g' /home/ocadmin/.bash_profile
+sed -i "s|sudo -i||g" /home/ocadmin/.bash_profile
+sed -i "s|mod_php5|mod_php7|g" $OCPATH/.htaccess
 cat /dev/null > /etc/rc.local
 cat << RCLOCAL > "/etc/rc.local"
 #!/bin/sh -e
