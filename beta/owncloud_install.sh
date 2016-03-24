c40dca61ff22e6a11c7602e9bb4d88949467bfd6
enoch85 <enoch85@gmail.com>
2016-03-24 23:56:01 +0100

    missed that one...

--- beta/owncloud_install.sh
+++ beta/owncloud_install.sh
@@ -4,8 +4,10 @@
 
 ## THIS IS FOR TESTING ##
 
+https://download.owncloud.org/community/testing/owncloud-9.0.1beta.zip
+
 OCVERSION=9.0.1beta
-OCPATH=https://download.owncloud.org/community/testing/$OCVERSION
+OCPATH=https://download.owncloud.org/community/testing/owncloud-$OCVERSION
 CONVER=v1.1.0.0
 CONVER_FILE=contacts.tar.gz
 CONVER_REPO=https://github.com/owncloud/contacts/releases/download
