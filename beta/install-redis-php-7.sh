33403b5240658b52be71658feb8159fab348d6f9
enoch85 <enoch85@gmail.com>
2016-03-28 23:32:16 +0200

    change redis stop command

--- beta/install-redis-php-7.sh
+++ beta/install-redis-php-7.sh
@@ -114,7 +114,7 @@ sed -i "s|port 6379|port 0|g" $REDIS_CONF
 sed -i "s|###############|SOCKET='$REDIS_SOCK'|g" $REDIS_INIT
 sed -i "s|REDISPORT shutdown|SOCKET shutdown|g" $REDIS_INIT
 sed -i "s|CLIEXEC -p|CLIEXEC -s|g" $REDIS_INIT
-service redis_6379 stop
+redis-cli SHUTDOWN
 
 # Cleanup
 apt-get purge -y \
